322b9cbe3d7c5dd2d8be4157cfdb80db5666e998
Little Fish <i529561@gmail.com>
2025-01-07 10:52:14 +0800

    Rewrite App to function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import './App.scss'
 import FileView from "./FileView.jsx";
-import React from "react";
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 import {far} from '@fortawesome/free-regular-svg-icons'
 import {fas} from '@fortawesome/free-solid-svg-icons'
+import {useComponentDidMount} from "./helper.jsx";
 
 library.add(fas, far, fab)
 
 // https://staging-v6.patternfly.org/
 
-class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
+function App() {
+    useComponentDidMount(() => {
         document.body.oncontextmenu = (evt) => {
             evt.preventDefault();
         }
-    }
+    })
 
-    render() {
-        return <>
-            <FileView />
-        </>
-    }
+    return <>
+        <FileView />
+    </>
 }
 
 export default App;
